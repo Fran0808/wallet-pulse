a772c7592cc5a1e4e9f262d3ce7fb73269c3eabd
Fran0808 <fortegaquipse@gmail.com>
2026-09-10 20:27:47 -0500

    feat(api): add automatic dotenv configuration

--- api/src/main/java/com/store/api/App.java
+++ api/src/main/java/com/store/api/App.java
@@ -3,11 +3,37 @@ package com.store.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
+        loadDotEnv();
         SpringApplication.run(App.class, args);
     }
 
+    private static void loadDotEnv() {
+        File[] candidateFiles = new File[]{
+                new File(".env"),
+                new File("../.env")
+        };
+        for (File file : candidateFiles) {
+            if (file.exists() && file.isFile()) {
+                Properties props = new Properties();
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    props.load(fis);
+                    props.forEach((key, value) -> {
+                        if (System.getProperty((String) key) == null && System.getenv((String) key) == null) {
+                            System.setProperty((String) key, (String) value);
+                        }
+                    });
+                    break;
+                } catch (IOException ignored) {}
+            }
+        }
+    }
 }
